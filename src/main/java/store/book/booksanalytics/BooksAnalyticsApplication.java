76d9aa85d096901adada9f82efe8c266a0008756
Alexander <tatrun89@gmail.com>
2021-06-25 20:32:46 +0300

    initial commit

---
+++ src/main/java/store/book/booksanalytics/BooksAnalyticsApplication.java
@@ -0,0 +1,13 @@
+package store.book.booksanalytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksAnalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksAnalyticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/store/book/booksanalytics/BooksAnalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package store.book.booksanalytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksAnalyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
